90719587ae46b2ff8c54e50f0509072babdd8f9c
mohammadibrahiim1 <mohammadibrahiim1@gmail.com>
2023-10-18 15:55:11 +0600

    create conversatin model schema

---
+++ models/conversationModel.js
@@ -0,0 +1,27 @@
+const { default: mongoose } = require("mongoose");
+
+const conversationSchema = mongoose.Schema(
+  {
+    creator: {
+      id: mongoose.Types.ObjectId,
+      name: String,
+      avatar: String,
+    },
+    participant: {
+      id: mongoose.Types.ObjectId,
+      name: String,
+      avatar: String,
+    },
+    last_updated: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const conversationModel = mongoose.model("conversation", conversationSchema);
+
+module.exports = conversationSchema;
